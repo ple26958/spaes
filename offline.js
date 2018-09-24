c89c34fc6849a308c16f0cf503791f45bf9e9e26
jetsadapron <pleton0803@gmail.com>
2018-09-24 17:54:38 +0700

    p

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1537786177,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/hero-sheet0.png",
+		"images/blulet-sheet0.png",
+		"images/enemy1-sheet0.png",
+		"images/box-sheet0.png",
+		"images/particles.png",
+		"images/enemy2-sheet0.png",
+		"images/enemy3-sheet0.png",
+		"images/enemy4-sheet0.png",
+		"images/item-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
